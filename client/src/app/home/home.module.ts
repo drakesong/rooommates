f0f8d27473efbf8da50575ba15ff1ac64f64ba55
Drake Song <drakesong216@gmail.com>
2019-04-09 02:12:29 -0700

    Removed unused import.

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { HomeRoutingModule } from './home-routing.module';
