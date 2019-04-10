f6c99d8cdb5436e615ec8180bcc314dee56beff2
Drake Song <drakesong216@gmail.com>
2019-04-09 18:17:26 -0700

    Changed convention.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,27 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: 'home',
-        loadChildren: './home/home.module#HomeModule'
-    },
-    {
-        path: 'chat',
-        loadChildren: './chat/chat.module#ChatModule'
-    },
-    {
-        path: 'explore',
-        loadChildren: './explore/explore.module#ExploreModule'
-    },
-    {
-        path: 'profile',
-        loadChildren: './profile/profile.module#ProfileModule'
-    },
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    }
+    { path: 'home', loadChildren: './home/home.module#HomeModule' },
+    { path: 'chat', loadChildren: './chat/chat.module#ChatModule' },
+    { path: 'explore', loadChildren: './explore/explore.module#ExploreModule' },
+    { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
