38473af8eab454dc83d7b854f6fd688e60fa12a1
Drake Song <drakesong216@gmail.com>
2019-04-09 23:19:22 -0700

    Regenerated register module and register-page component with --routing option.

--- client/src/app/register/register.module.ts
+++ client/src/app/register/register.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [RegisterPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RegisterRoutingModule
   ]
 })
 export class RegisterModule { }
