defaa6dadb635936eba0b2c4ba8a68d3a7198e89
Drake Song <drakesong216@gmail.com>
2019-04-10 05:18:29 -0700

    Corrected spelling.

--- client/src/app/sign-in/sign-in.module.ts
+++ client/src/app/sign-in/sign-in.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SingInPageComponent } from './sing-in-page/sing-in-page.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 
 @NgModule({
-  declarations: [SingInPageComponent],
+  declarations: [SingInPageComponent, SignInPageComponent],
   imports: [
     CommonModule,
     SignInRoutingModule
