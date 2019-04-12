8c89784d1b298db44be54f1cd5883bbf9b4337c2
Drake Song <drakesong216@gmail.com>
2019-04-11 17:00:34 -0700

    Created basic register function.

---
+++ server/src/main/java/roommates/controllers/RegisterController.java
@@ -0,0 +1,91 @@
+package com.roommates.roommates;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import org.json.*;
+
+import javax.servlet.http.*;
+
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+import java.security.*;
+
+@RestController
+public class RegisterController {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/Roommates";
+    static Connection conn = null;
+
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+    public ResponseEntity<String> register(@RequestBody String body, HttpServletRequest request) {
+        PreparedStatement ps = null;
+        initializeDBConnection();
+
+		try {
+			JSONObject bodyObj = new JSONObject(body);
+            String hashedPassword = hashPassword(bodyObj.getString("password"));
+
+            String query = "INSERT INTO Users (email, password, first_name, last_name) VALUES (?, ?, ?, ?)";
+            ps = conn.prepareStatement(query);
+
+            ps.setString(1, bodyObj.getString("email"));
+            ps.setString(2, hashedPassword);
+            ps.setString(3, bodyObj.getString("first_name"));
+            ps.setString(4, bodyObj.getString("last_name"));
+
+            ps.executeUpdate();
+            ps.close();
+		} catch(JSONException e) {
+			e.printStackTrace();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+		return new ResponseEntity(null, null, HttpStatus.BAD_REQUEST);
+	}
+
+    public static void initializeDBConnection() {
+        try {
+            File file = new File("../src/main/java/roommates/configs/dbparams.txt");
+            Scanner sc = new Scanner(file);
+            final String USER = sc.nextLine();
+            final String PASSWORD = sc.nextLine();
+            sc. close();
+
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String hashPassword(String password) {
+        MessageDigest digest = null;
+		String hashedPassword = null;
+
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+			hashedPassword = bytesToHex(digest.digest(password.getBytes("UTF-8")));
+
+		} catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+		} catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+		}
+
+        return hashedPassword;
+    }
+
+    public static String bytesToHex(byte[] in) {
+		StringBuilder builder = new StringBuilder();
+		for(byte b: in) {
+			builder.append(String.format("%02x", b));
+		}
+		return builder.toString();
+	}
+}
