b1bf7deb87fa73ce1812bb9ddbb76907a580094a
Drake Song <drakesong216@gmail.com>
2019-04-30 09:52:07 -0700

    Implemented subscription on signin/signout button on navbar.

--- client/src/app/shared/auth-guard.service.ts
+++ client/src/app/shared/auth-guard.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { JwtService } from './jwt.service';
+import { Observable, of as observableOf, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
+  public getLoggedInStatus = new Subject();
 
   constructor(private jwtService: JwtService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     if (this.jwtService.loggedIn) {
-      return true;
+      this.getLoggedInStatus.next('Sign Out');
+      return observableOf(true);
     }
 
+    this.getLoggedInStatus.next('Sign In');
     this.router.navigate(['home']);
-    return false;
+    alert("You must be logged in.");
+    return observableOf(false);
+  }
+
+  checkStatus(): void {
+    if (this.jwtService.loggedIn) {
+      this.getLoggedInStatus.next('Sign Out');
+    } else {
+      this.getLoggedInStatus.next('Sign In');
+    }
   }
 }
