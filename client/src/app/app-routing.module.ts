aa542ce45fda5bf45f1231c7fa761ffd2ca148b0
Drake Song <drakesong216@gmail.com>
2019-05-04 06:33:10 -0700

    Extended AuthGuard to explore page.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuardService } from './shared/auth-guard.service';
 const routes: Routes = [
     { path: 'home', loadChildren: './home/home.module#HomeModule', data: {animation: 'Home'} },
     { path: 'chat', loadChildren: './chat/chat.module#ChatModule', data: {animation: 'Chat'} },
-    { path: 'explore', loadChildren: './explore/explore.module#ExploreModule', data: {animation: 'Explore'} },
+    { path: 'explore', loadChildren: './explore/explore.module#ExploreModule', data: {animation: 'Explore'}, canActivate: [AuthGuardService] },
     { path: 'profile', loadChildren: './profile/profile.module#ProfileModule', data: {animation: 'Profile'}, canActivate: [AuthGuardService] },
     { path: 'register', loadChildren: './register/register.module#RegisterModule', data: {animation: 'Register'} },
     { path: 'sign-in', loadChildren: './sign-in/sign-in.module#SignInModule', data: {animation: 'SignIn'} },
